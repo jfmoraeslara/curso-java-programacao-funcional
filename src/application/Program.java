51313b2ad97f1b7e55d06bc3f6d4c58542f89d24
João Fernando <jfmoraeslara@gmail.com>
2022-02-19 14:23:14 -0300

    Usando uma sintaxe de classe anônima

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -10,16 +11,23 @@ public class Program {
 	public static void main(String[] args) {
 		
 		List<Product> list = new ArrayList<>();
-		
+				
 		list.add(new Product("Tv", 900.0));
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 400.0));
 		
-		list.sort(new MyComparator());
+		//declarando uma variável usando uma sintaxe de classe anônima
+		Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}			
+		};
+		
+		list.sort(comp);
 		
 		for (Product p : list) {
 			System.out.println(p);
 		}
 	}
-
 }
